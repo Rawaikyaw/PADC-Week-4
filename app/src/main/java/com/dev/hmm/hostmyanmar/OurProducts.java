3cb066e2560b2cc8ee99ada1654083b7cb399e3f
Rawaikyaw <wansurawai@gmail.com>
2016-07-01 22:54:55 +0630

    Week - 4

---
+++ app/src/main/java/com/dev/hmm/hostmyanmar/OurProducts.java
@@ -0,0 +1,13 @@
+package com.dev.hmm.hostmyanmar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OurProducts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_our_products);
+    }
+}
---
+++ app/src/main/java/com/dev/hmm/hostmyanmar/about_us.java
@@ -0,0 +1,20 @@
+package com.dev.hmm.hostmyanmar;
+
+import android.content.Intent;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class about_us extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+    }
+
+    
+
+}
